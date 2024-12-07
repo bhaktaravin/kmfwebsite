e55f2e33d28d8bd6db8adadd0e569ac833ea6e3d
bhaktaravin <ravin.bhakta@gmail.com>
2024-12-07 10:53:04 -0800

    Updated Files

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
-import Services from './pages/Services'
+import Team from './pages/Team'
+import InMemory from './pages/InMemory'
 import Contact from './pages/Contact'
 import Donate from './pages/Donate'
-import Team from './pages/Team'
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/in-memory" element={<InMemory />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/donate" element={<Donate />} />
-            <Route path="/team" element={<Team />} />
           </Routes>
         </main>
         <Footer />
